44de02f6bdf798cc597992609866b39a9329eb44
Tucker Barrett <tuckerbarrett@gmail.com>
2021-10-29 17:37:17 -0400

    Fixes overflow issue in Safari

--- src/components/Burger.styled.js
+++ src/components/Burger.styled.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const StyledBurger = styled.button`
   position: absolute;
-  top: 5%;
+  top: 2rem;
   right: 2rem;
   display: none;
   flex-direction: column;
