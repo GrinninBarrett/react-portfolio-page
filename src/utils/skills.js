1997bb9324a17b703e8abe4a7a663fa33c89fa2e
Tucker Barrett <tuckerbarrett@gmail.com>
2021-11-16 17:59:11 -0500

    Installs carousel and adds initial skills

---
+++ src/utils/skills.js
@@ -0,0 +1,22 @@
+const skills = [
+    {
+        id: 1,
+        name: "React",
+        icon: "https://res.cloudinary.com/dydjenltr/image/upload/v1637103246/Portfolio%20Images/Skills%20Icons/react_pmwwzt.svg",
+        url: "https://reactjs.org/"
+    },
+    {
+        id: 2,
+        name: "MongoDB",
+        icon: "https://res.cloudinary.com/dydjenltr/image/upload/v1637103399/Portfolio%20Images/Skills%20Icons/mongodb_sekoab.svg",
+        url: "https://www.mongodb.com/"
+    },
+    {
+        id: 3,
+        name: "Express",
+        icon: "https://res.cloudinary.com/dydjenltr/image/upload/v1637103497/Portfolio%20Images/Skills%20Icons/express_lsrbj0.svg",
+        url: "http://expressjs.com/"
+    }
+]
+
+export { skills };
