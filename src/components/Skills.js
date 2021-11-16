e4448b3d01f4a58792f5f402dce979ac4effeadd
Tucker Barrett <tuckerbarrett@gmail.com>
2021-11-16 18:05:35 -0500

    Adds next bit to Skills component

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,11 +1,20 @@
 import React from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
 import { skills } from "../utils/skillsList";
 
-const Skills = ({ name, image, url }) => {
+const Skills = () => {
+  const handleDragStart = (e) => e.preventDefault();
 
-    return (
-
-    );
+  return (
+    <section>
+      {skills.map((skill) => (
+        <a href={skill.url}>
+          <img src={skill.image} alt={skill.name} onDragStart={handleDragStart} role="presentation"></img>
+        </a>
+      ))}
+    </section>
+  );
 };
 
 export default Skills;
