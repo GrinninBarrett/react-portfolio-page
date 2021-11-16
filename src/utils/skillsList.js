7c34d10c8350bb12f6efda7584824a1950954a71
Tucker Barrett <tuckerbarrett@gmail.com>
2021-11-16 18:29:45 -0500

    Adds GraphQL and Node.js to skillsList

--- src/utils/skillsList.js
+++ src/utils/skillsList.js
@@ -16,6 +16,18 @@ const skills = [
         name: "Express",
         image: "https://res.cloudinary.com/dydjenltr/image/upload/v1637103497/Portfolio%20Images/Skills%20Icons/express_lsrbj0.svg",
         url: "http://expressjs.com/"
+    },
+    {
+        id: 4,
+        name: "GraphQL",
+        image: "https://res.cloudinary.com/dydjenltr/image/upload/v1637105254/Portfolio%20Images/Skills%20Icons/graphql_pkxlbx.svg",
+        url: "https://graphql.org/"
+    },
+    {
+        id: 5,
+        name: "Node.js",
+        image: "https://res.cloudinary.com/dydjenltr/image/upload/v1637105316/Portfolio%20Images/Skills%20Icons/nodejs_q2dhd5.svg",
+        url: "https://nodejs.org/en/"
     }
 ]
 
