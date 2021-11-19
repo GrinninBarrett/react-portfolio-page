7692dbbca807a55164f85e9d24df1a40b9526c76
Tucker Barrett <tuckerbarrett@gmail.com>
2021-11-19 17:29:48 -0500

    Adds more skills to skillsList

--- src/utils/skillsList.js
+++ src/utils/skillsList.js
@@ -28,7 +28,37 @@ const skills = [
         name: "Node.js",
         image: "https://res.cloudinary.com/dydjenltr/image/upload/v1637105316/Portfolio%20Images/Skills%20Icons/nodejs_q2dhd5.svg",
         url: "https://nodejs.org/en/"
-    }
+    },
+    {
+        id: 6,
+        name: "HTML",
+        image: "https://res.cloudinary.com/dydjenltr/image/upload/v1637360637/Portfolio%20Images/Skills%20Icons/html-5_shbqqb.svg",
+        url: "https://html.spec.whatwg.org/multipage/"
+    },
+    {
+        id: 7,
+        name: "CSS",
+        image: "https://res.cloudinary.com/dydjenltr/image/upload/v1637360618/Portfolio%20Images/Skills%20Icons/css-3_bl1bth.svg",
+        url: "https://www.w3.org/TR/CSS/"
+    },
+    {
+        id: 8,
+        name: "jQuery",
+        image: "https://res.cloudinary.com/dydjenltr/image/upload/v1637360689/Portfolio%20Images/Skills%20Icons/jquery_ojbebr.svg",
+        url: "https://jquery.com/"
+    },
+    {
+        id: 9,
+        name: "Heroku",
+        image: "https://res.cloudinary.com/dydjenltr/image/upload/v1637360282/Portfolio%20Images/Skills%20Icons/heroku-icon_ojdcc6.svg",
+        url: "https://heroku.com"
+    },
+    {
+        id: 10,
+        name: "Mongoose",
+        image: "https://res.cloudinary.com/dydjenltr/image/upload/c_scale,h_89,q_auto:eco/v1637360904/Portfolio%20Images/Skills%20Icons/mongoose-logo_mcynyc.png",
+        url: "https://mongoosejs.com/"
+    },
 ]
 
 export { skills };
