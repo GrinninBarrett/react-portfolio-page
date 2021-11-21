f831fe83b0cad9a450aa65fdb1213bf161707e5d
Tucker Barrett <tuckerbarrett@gmail.com>
2021-11-21 10:39:05 -0500

    Adds skills component to About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Skills from "../components/Skills";
 
 const About = () => {
   return (
@@ -17,19 +18,23 @@ const About = () => {
             <h1 className="base-text">A bit about me</h1>
             <p className="base-text">
               My background is education - I have taught music for 4 years, with
-              a 4-year break in the middle to live in South Korea and teach ESL.
+              a 4-year break from music teaching to teach ESL in South Korea.
               While music is something I plan to enjoy forever, I realized some
               time ago that public school teaching is not.
             </p>
             <p className="base-text">
-              As someone who always loved tech, but was too deeply involved in
+              As someone who always loved technology, but was too deeply involved in
               the pursuit of a career in music, I long viewed the field of
-              technology as something not meant for a musical creative like me.
-              I've realized, now, that I was very wrong. Now, rather than simply
-              benefiting from advances in technology, I plan to contribute.
+              technology as something not meant for a musician like me.
+              Now, rather than simply benefiting from advances in technology, I plan to contribute.
             </p>
           </div>
         </div>
+      </section>
+
+      <section id="skills-section" className="flex-item flex-column">
+        <h2 className="base-text">Some of my skills</h2>
+        <Skills />
       </section>
 
       <section className="container flex-item flex-column">
