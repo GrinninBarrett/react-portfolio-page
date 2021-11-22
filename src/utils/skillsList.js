35a6b8cbfbadb7272fe66f886d30a5d1b8ee8449
Tucker Barrett <tuckerbarrett@gmail.com>
2021-11-22 15:06:01 -0500

    Reorders skillsList and adds Handlebars

--- src/utils/skillsList.js
+++ src/utils/skillsList.js
@@ -1,70 +1,66 @@
 const skills = [
     {
-        id: 1,
-        name: "React",
-        image: "https://res.cloudinary.com/dydjenltr/image/upload/v1637103246/Portfolio%20Images/Skills%20Icons/react_pmwwzt.svg",
-        url: "https://reactjs.org/"
-    },
-    {
-        id: 2,
-        name: "MongoDB",
-        image: "https://res.cloudinary.com/dydjenltr/image/upload/v1637103399/Portfolio%20Images/Skills%20Icons/mongodb_sekoab.svg",
-        url: "https://www.mongodb.com/"
-    },
-    {
-        id: 3,
-        name: "Express.js",
-        image: "https://res.cloudinary.com/dydjenltr/image/upload/v1637103497/Portfolio%20Images/Skills%20Icons/express_lsrbj0.svg",
-        url: "http://expressjs.com/"
-    },
-    {
-        id: 4,
-        name: "GraphQL",
-        image: "https://res.cloudinary.com/dydjenltr/image/upload/v1637105254/Portfolio%20Images/Skills%20Icons/graphql_pkxlbx.svg",
-        url: "https://graphql.org/"
-    },
-    {
-        id: 5,
-        name: "Node.js",
-        image: "https://res.cloudinary.com/dydjenltr/image/upload/v1637105316/Portfolio%20Images/Skills%20Icons/nodejs_q2dhd5.svg",
-        url: "https://nodejs.org/en/"
-    },
-    {
-        id: 6,
-        name: "HTML",
-        image: "https://res.cloudinary.com/dydjenltr/image/upload/v1637360637/Portfolio%20Images/Skills%20Icons/html-5_shbqqb.svg",
-        url: "https://html.spec.whatwg.org/multipage/"
-    },
-    {
-        id: 7,
-        name: "CSS",
-        image: "https://res.cloudinary.com/dydjenltr/image/upload/v1637360618/Portfolio%20Images/Skills%20Icons/css-3_bl1bth.svg",
-        url: "https://www.w3.org/TR/CSS/"
-    },
-    {
-        id: 8,
-        name: "jQuery",
-        image: "https://res.cloudinary.com/dydjenltr/image/upload/v1637360689/Portfolio%20Images/Skills%20Icons/jquery_ojbebr.svg",
-        url: "https://jquery.com/"
-    },
-    {
-        id: 9,
-        name: "Heroku",
-        image: "https://res.cloudinary.com/dydjenltr/image/upload/v1637360282/Portfolio%20Images/Skills%20Icons/heroku-icon_ojdcc6.svg",
-        url: "https://heroku.com"
-    },
-    {
-        id: 10,
         name: "JavaScript",
         image: "https://res.cloudinary.com/dydjenltr/image/upload/v1637361118/Portfolio%20Images/Skills%20Icons/javascript_o8zgz0.svg",
         url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
     },
     {
-        id: 11,
+        name: "React",
+        image: "https://res.cloudinary.com/dydjenltr/image/upload/v1637103246/Portfolio%20Images/Skills%20Icons/react_pmwwzt.svg",
+        url: "https://reactjs.org/"
+    },
+    {
+        name: "MongoDB",
+        image: "https://res.cloudinary.com/dydjenltr/image/upload/v1637103399/Portfolio%20Images/Skills%20Icons/mongodb_sekoab.svg",
+        url: "https://www.mongodb.com/"
+    },
+    {
+        name: "Express.js",
+        image: "https://res.cloudinary.com/dydjenltr/image/upload/v1637103497/Portfolio%20Images/Skills%20Icons/express_lsrbj0.svg",
+        url: "http://expressjs.com/"
+    },
+    {
+        name: "GraphQL",
+        image: "https://res.cloudinary.com/dydjenltr/image/upload/v1637105254/Portfolio%20Images/Skills%20Icons/graphql_pkxlbx.svg",
+        url: "https://graphql.org/"
+    },
+    {
+        name: "Node.js",
+        image: "https://res.cloudinary.com/dydjenltr/image/upload/v1637105316/Portfolio%20Images/Skills%20Icons/nodejs_q2dhd5.svg",
+        url: "https://nodejs.org/en/"
+    },
+    {
+        name: "HTML",
+        image: "https://res.cloudinary.com/dydjenltr/image/upload/v1637360637/Portfolio%20Images/Skills%20Icons/html-5_shbqqb.svg",
+        url: "https://html.spec.whatwg.org/multipage/"
+    },
+    {
+        name: "CSS",
+        image: "https://res.cloudinary.com/dydjenltr/image/upload/v1637360618/Portfolio%20Images/Skills%20Icons/css-3_bl1bth.svg",
+        url: "https://www.w3.org/TR/CSS/"
+    },
+    {
         name: "Bootstrap",
         image: "https://res.cloudinary.com/dydjenltr/image/upload/c_scale,h_154,q_auto/v1637610959/Portfolio%20Images/Skills%20Icons/bootstrap_wpojga.svg",
         url: "https://getbootstrap.com/"
     },
+    {
+        name: "jQuery",
+        image: "https://res.cloudinary.com/dydjenltr/image/upload/v1637360689/Portfolio%20Images/Skills%20Icons/jquery_ojbebr.svg",
+        url: "https://jquery.com/"
+    },
+    {
+        name: "Heroku",
+        image: "https://res.cloudinary.com/dydjenltr/image/upload/v1637360282/Portfolio%20Images/Skills%20Icons/heroku-icon_ojdcc6.svg",
+        url: "https://heroku.com"
+    },
+    {
+        name: "Handlebars",
+        image: "https://res.cloudinary.com/dydjenltr/image/upload/c_fit,h_512,w_512/v1637611227/Portfolio%20Images/Skills%20Icons/handlebars_d3xi5i.svg",
+        url: "https://handlebarsjs.com/"
+    }
+
+
 ]
 
 export { skills };
