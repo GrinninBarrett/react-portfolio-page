ecd45e3e530c0706becb3ed2b95397a18e2eec2b
Tucker Barrett <tuckerbarrett@gmail.com>
2022-01-02 20:10:50 -0500

    Moves Header back, adds styles to Logo and Nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,59 +1,76 @@
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 
 const Header = () => {
+  const StyledNav = styled.nav`
+    justify-content: space-between;
+    height: 100%;
+    padding: 1em 0;
+  `;
+
+  const StyledLogo = styled(Link)`
+    &:hover {
+      color: var(--green-accent);
+    }
+  `;
+
   return (
     <header>
       <div className="container">
-        <nav id="main-nav" className="flex-item nav-links-container">
-          <Link id="header-logo" className="logo base-text" to="/">
+        <StyledNav id="main-nav" className="flex-item nav-links-container">
+          <StyledLogo id="header-logo" className="logo base-text" to="/">
             T
-          </Link>
+          </StyledLogo>
           <div id="nav-links">
             <div id="nav-menu" className="flex-item nav-list">
               <NavLink
                 className="main-nav-item nav-item link green-link base-text"
-                exact to="/"
+                exact
+                to="/"
                 activeStyle={{
-                  backgroundColor: "#47684a",
-                  color: "rgb(238, 238, 238)"
+                  backgroundColor: '#47684a',
+                  color: 'rgb(238, 238, 238)',
                 }}
               >
                 Home
               </NavLink>
               <NavLink
                 className="main-nav-item nav-item link green-link base-text"
-                exact to="/about"
+                exact
+                to="/about"
                 activeStyle={{
-                  backgroundColor: "#47684a",
-                  color: "rgb(238, 238, 238)"
+                  backgroundColor: '#47684a',
+                  color: 'rgb(238, 238, 238)',
                 }}
               >
                 About
               </NavLink>
               <NavLink
                 className="main-nav-item nav-item link green-link base-text"
-                exact to="/projects"
+                exact
+                to="/projects"
                 activeStyle={{
-                  backgroundColor: "#47684a",
-                  color: "rgb(238, 238, 238)"
+                  backgroundColor: '#47684a',
+                  color: 'rgb(238, 238, 238)',
                 }}
               >
                 Projects
               </NavLink>
               <NavLink
                 className="main-nav-item nav-item link green-link base-text"
-                exact to="/contact"
+                exact
+                to="/contact"
                 activeStyle={{
-                  backgroundColor: "#47684a",
-                  color: "rgb(238, 238, 238)"
+                  backgroundColor: '#47684a',
+                  color: 'rgb(238, 238, 238)',
                 }}
               >
                 Contact
               </NavLink>
             </div>
           </div>
-        </nav>
+        </StyledNav>
       </div>
     </header>
   );
