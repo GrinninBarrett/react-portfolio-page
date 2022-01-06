6bed6b8c6aac849a76922cfa5145ca2d2c51614a
Tucker Barrett <tuckerbarrett@gmail.com>
2022-01-05 21:24:17 -0500

    Moves styled components out of component function

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,92 +2,92 @@ import React from 'react';
 import styled from 'styled-components';
 import Skills from '../components/Skills';
 
+// Styled Components
+const StyledMain = styled.main`
+  min-height: 80vh;
+`;
+
+const StyledAboutIntro = styled.section`
+  justify-content: space-evenly;
+  margin: 4em auto;
+
+  @media screen and (max-width: 768px) {
+    height: auto;
+    margin: 2em auto;
+  }
+`;
+
+const StyledTopIntroContainer = styled.div`
+  width: 85%;
+  justify-content: space-between;
+
+  @media screen and (max-width: 992px) {
+    width: 100%;
+  }
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const StyledTuckerPhoto = styled.img`
+  height: 350px;
+  width: 350px;
+
+  @media screen and (max-width: 992px) {
+    height: 300px;
+    width: 300px;
+  }
+
+  @media screen and (max-height: 750px) {
+    height: 300px;
+    width: 300px;
+  }
+`;
+
+const StyledSkillsSection = styled.section`
+  background-color: rgb(244, 245, 244);
+  margin: 6rem 0 4rem 0;
+  padding: 0 0 2rem 0;
+`;
+
+const StyledAboutTextContainer = styled.div`
+  width: 50%;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+const StyledBottomOfPageLinkContainer = styled.section`
+  width: 50%;
+  margin: 6rem auto;
+`;
+
+const StyledBottomOfPageLink = styled.a`
+  background-color: var(--green-accent);
+  color: var(--background);
+  padding: 8px 0;
+  border-radius: 20px;
+  width: 30%;
+  text-align: center;
+
+  &:hover {
+    background-color: var(--medium-green-accent);
+  }
+
+  @media screen and (max-width: 992px) {
+    width: 60%;
+  }
+
+  @media screen and (max-width: 768px) {
+    margin: 1rem auto;
+    width: 100%;
+  }
+`;
+
+// Component function
 const About = () => {
-  // Styled Components
-
-  const StyledMain = styled.main`
-    min-height: 80vh;
-  `;
-
-  const StyledAboutIntro = styled.section`
-    justify-content: space-evenly;
-    margin: 4em auto;
-
-    @media screen and (max-width: 768px) {
-      height: auto;
-      margin: 2em auto;
-    }
-  `;
-
-  const StyledTopIntroContainer = styled.div`
-    width: 85%;
-    justify-content: space-between;
-
-    @media screen and (max-width: 992px) {
-      width: 100%;
-    }
-
-    @media screen and (max-width: 768px) {
-      flex-direction: column;
-    }
-  `;
-
-  const StyledTuckerPhoto = styled.img`
-    height: 350px;
-    width: 350px;
-
-    @media screen and (max-width: 992px) {
-      height: 300px;
-      width: 300px;
-    }
-
-    @media screen and (max-height: 750px) {
-      height: 300px;
-      width: 300px;
-    }
-  `;
-
-  const StyledSkillsSection = styled.section`
-    background-color: rgb(244, 245, 244);
-    margin: 6rem 0 4rem 0;
-    padding: 0 0 2rem 0;
-  `;
-
-  const StyledAboutTextContainer = styled.div`
-    width: 50%;
-
-    @media screen and (max-width: 768px) {
-      width: 100%;
-    }
-  `;
-
-  const StyledBottomOfPageLinkContainer = styled.section`
-    width: 50%;
-    margin: 6rem auto;
-  `;
-
-  const StyledBottomOfPageLink = styled.a`
-    background-color: var(--green-accent);
-    color: var(--background);
-    padding: 8px 0;
-    border-radius: 20px;
-    width: 30%;
-    text-align: center;
-
-    &:hover {
-      background-color: var(--medium-green-accent);
-    }
-
-    @media screen and (max-width: 992px) {
-      width: 60%;
-    }
-
-    @media screen and (max-width: 768px) {
-      margin: 1rem auto;
-      width: 100%;
-    }
-  `;
-
   return (
     <StyledMain>
       <StyledAboutIntro id="about-intro" className="intro container flex-item">
